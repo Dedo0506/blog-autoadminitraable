c4229326eac8068c7c2b9d9139a0845271414c49
DanielaDavila <danielaelizabethdavaila@gmail.com>
2023-05-25 14:55:44 -0600

    creacion de tablas, modelos y relaciones

---
+++ app/Models/Post.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use PhpParser\Node\Expr\FuncCall;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    //relacion 1:N inversa
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function categoria(){
+        return $this->belongsTo(Categoria::class);
+    }
+
+    //relacion N:N 
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
+
+    //relacion 1:1 polimorfica
+    public function image(){
+        return $this->morphOne(Image::class, 'imagable');
+    }
+
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,10 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    //relacion 1:N
+
+    public function posts(){
+        return $this->hasMany(Post::class);
+    }
 }
