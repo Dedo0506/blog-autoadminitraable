1ac57516a9b94544c2d50bc1fc8ddc9a6466a404
DanielaDavila <danielaelizabethdavaila@gmail.com>
2023-05-25 15:21:42 -0600

    Creacion Factorys

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Categoria;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $name =$this->faker->unique->sentence();
+
+        return [
+            //
+            'name'=>$name,
+            'slug'=>Str::slug($name),
+            'extrac'=>$this->faker->text(250),
+            'body'=>$this->faker->text(250),
+            'status'=>$this->faker->randomElement([1,2]),
+            'Categoria_id'=>Categoria::all()->random()->id,
+            'User_id'=>User::all()->random()->id,
+
+        ];
+    }
+}
