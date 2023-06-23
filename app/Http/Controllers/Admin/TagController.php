a5823a154f8dab3535250f79547793e86bcf9f8f
DanielaDavila <danielaelizabethdavaila@gmail.com>
2023-06-23 15:28:53 -0600

    creando crud de tags

---
+++ app/Http/Controllers/Admin/TagController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Tag;
+use Illuminate\Http\Request;
+
+class TagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tags = Tag::all();
+      return view('admin.tags.index', compact('tags'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+      return view('admin.tags.create', compact(''));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+      return redirect()->route('')->with('','');  
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $tag)
+    {
+       return view('admin.tags.show', compact('tag'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $tag)
+    {
+        return view('admin.tags.edit', compact('tag'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $tag)
+    {
+      return redirect()->route('')->with('','');  
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $tag)
+    {
+       return redirect()->route('')->with('','');     
+    }
+}
--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,10 @@
 
 use App\Http\Controllers\Admin\CategoriaController;
 use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Admin\TagController;
 use Illuminate\Support\Facades\Route;
 Route::get('',[HomeController::class, 'index'])->name('admin.home');
+
 Route::resource('categorias', CategoriaController::class)->names('admin.categorias');
+
+Route::resource('tags', TagController::class)->names('admin.tags');
