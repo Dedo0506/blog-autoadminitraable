ae92ecdee9c260cbe78dc8009c93007e6aee92d5
DanielaDavila <danielaelizabethdavaila@gmail.com>
2023-08-09 16:10:49 -0600

    probando show-more

---
+++ resources/views/livewire/show-more-component.blade.php
@@ -0,0 +1,17 @@
+<div>
+    @php
+        $primeras_palabras = implode(' ', array_slice(str_word_count($post->body, 1), 0, 20));
+    @endphp
+
+    <p>{{ $primeras_palabras }}
+        @if ($post->id === $postToShowMore)
+            {{ substr($post->body, strlen($primeras_palabras)) }}
+        @endif
+        @if ($post->id !== $postToShowMore)
+            <span wire:click="toggleDescription({{ $post->id }})" style="cursor: pointer;" class="text-blue-500 hover:underline">... Mostrar más</span>
+        @else
+            <span wire:click="toggleDescription({{ $post->id }})" style="cursor: pointer;" class="text-blue-500 hover:underline">Ocultar</span>
+        @endif
+
+    </p>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 
 
+
 Route::get('/', [PostController::class, 'index'])->name('posts.index');
 
 Route::get('post/{post}', [PostController::class, 'show'])->name('posts.show');
