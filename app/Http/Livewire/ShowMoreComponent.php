6141dcc731f016d273fb479d980f5b9c8e888202
DanielaDavila <danielaelizabethdavaila@gmail.com>
2023-08-17 13:53:20 -0600

    mostrar y ocultar terminado

--- app/Http/Livewire/ShowMoreComponent.php
+++ app/Http/Livewire/ShowMoreComponent.php
@@ -6,7 +6,7 @@ use Livewire\Component;
 
 class ShowMoreComponent extends Component
 {
-    public $posts;
+    public $post;
 
     public $postToShowMore = null;
 
--- resources/views/livewire/show-more-component.blade.php
+++ resources/views/livewire/show-more-component.blade.php
@@ -1,19 +1,18 @@
 <div>
-    @foreach ($posts as $post)
-        @php
-            $primeras_palabras = implode(' ', array_slice(str_word_count($post->body, 1), 0, 20));
-        @endphp
+    @php
+        $primeras_palabras = implode(' ', array_slice(str_word_count($post->body, 1), 0, 20));
+    @endphp
 
-        <p>{{ $primeras_palabras }}
-            @if ($post->id === $postToShowMore)
-                {{ substr($post->body, strlen($primeras_palabras)) }}
-            @endif
-            @if ($post->id !== $postToShowMore)
-                <span wire:click="toggleDescription({{ $post->id }})" style="cursor: pointer;" class="text-blue-500 hover:underline">... Mostrar más</span>
-            @else
-                <span wire:click="toggleDescription({{ $post->id }})" style="cursor: pointer;" class="text-blue-500 hover:underline">Ocultar</span>
-            @endif
+    <p>{{ $primeras_palabras }}
+        @if ($post->id === $postToShowMore)
+            {{ substr($post->body, strlen($primeras_palabras)) }}
+        @endif
+        @if ($post->id !== $postToShowMore)
+            <span wire:click="toggleDescription({{ $post->id }})" style="cursor: pointer;" class="text-blue-500 hover:underline">... Mostrar más</span>
+        @else
+            <span wire:click="toggleDescription({{ $post->id }})" style="cursor: pointer;" class="text-blue-500 hover:underline">Ocultar</span>
+        @endif
 
-        </p>
-    @endforeach
+    </p>
+    
 </div>
